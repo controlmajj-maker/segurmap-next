7dcbcc045deb4ed8f1e92d0ebf025d8b22f918f5
controlmajj-maker <controlmajj@gmail.com>
2026-02-23 11:36:03 -0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,3 @@
-import { Pool } from "pg";
+import { neon } from "@neondatabase/serverless";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export default pool;
+export const sql = neon(process.env.DATABASE_URL!);
